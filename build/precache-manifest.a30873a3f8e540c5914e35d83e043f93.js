b296bbdec1a3e512bfda33ddc1b0ce958bf15538
snehakweera77 <snehakweera77@gmail.com>
2021-08-18 18:59:06 +0530

    build

---
+++ build/precache-manifest.a30873a3f8e540c5914e35d83e043f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12ac48629326c59caab4eb8c375bd38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b57af31b79927297583f",
+    "url": "/static/css/main.a7a74fcd.chunk.css"
+  },
+  {
+    "revision": "9cbe13e10c9f113485c1",
+    "url": "/static/js/2.e96df388.chunk.js"
+  },
+  {
+    "revision": "b57af31b79927297583f",
+    "url": "/static/js/main.db52cc7a.chunk.js"
+  },
+  {
+    "revision": "f1b768174fa530eb5144",
+    "url": "/static/js/runtime-main.39dd3371.js"
+  }
+]);
